74d0aefcae4ba9074717a8be2c8a54f66a265262
Rastovic <79258442+Rastovic@users.noreply.github.com>
2023-01-07 20:31:36 +0100

    lepsi search

--- apoteka_final/src/main/java/com/online/apoteka_final/Config/MyDataRestConfig.java
+++ apoteka_final/src/main/java/com/online/apoteka_final/Config/MyDataRestConfig.java
@@ -1,4 +1,4 @@
-package com.online.apoteka_final.config;
+package com.online.apoteka_final.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;

2c4b42f2a12c8e0c29222265309463cb89dce37b
Rastovic <79258442+Rastovic@users.noreply.github.com>
2023-01-10 21:54:14 +0100

    adresa

--- apoteka_final/src/main/java/com/online/apoteka_final/dao/AddressRepository.java
+++ apoteka_final/src/main/java/com/online/apoteka_final/dao/AddressRepository.java
@@ -1,11 +1,6 @@
 package com.online.apoteka_final.dao;
 
-import java.util.List;
-import java.util.Optional;
 
-import org.hibernate.criterion.Order;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
